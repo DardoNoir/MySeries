5dac6ee57c1c63917fe305664394480a2b285ddf
Dardo <dardonoir@gmail.com>
2025-09-27 09:57:00 -0300

    Eliminar front que no funciona

--- angular/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { SeriesSearch } from './components/series-search/series-search';
-
-export const routes: Routes = [
-  { path: 'series-search', component: SeriesSearch },
-  { path: '', redirectTo: 'series-search', pathMatch: 'full' } // opcional: home = búsqueda
-];
