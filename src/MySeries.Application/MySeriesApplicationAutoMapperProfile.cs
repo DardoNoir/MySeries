8c8357224922f7fb634792fd622d1c2e3f275775
Dardo Noir <noird@o365frcuutneduar.onmicrosoft.com>
2026-01-12 16:44:38 -0300

    .

--- src/MySeries.Application/MySeriesApplicationAutoMapperProfile.cs
+++ src/MySeries.Application/MySeriesApplicationAutoMapperProfile.cs
@@ -10,5 +10,6 @@ public class MySeriesApplicationAutoMapperProfile : Profile
         /* You can configure your AutoMapper mapping configuration here.
          * Alternatively, you can split your mapping configurations
          * into multiple profile classes for a better organization. */
+
     }
 }
